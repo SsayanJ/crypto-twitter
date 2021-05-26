cd1052424767566195b16f32c1f91fa101f3168d
Jeff <jean-francois.benhaim@epita.fr>
2021-05-26 14:35:49 +0200

    replacing the way to check the owner of the tweet

--- contracts/TwitterClone.sol
+++ contracts/TwitterClone.sol
@@ -5,13 +5,22 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TwitterClone is Ownable {
-    event NewTweet(uint256 tweetId, string tweetText);
+    event NewTweet(uint256 tweetId, string tweetText, address owner);
 
     struct Tweet{
         string tweetText;
         bool deleted;
     }
     Tweet[] public tweets;
+    mapping(uint256 => address) public tweetToOwner;
+    mapping(address => uint256) ownerTweetCount;
+
+    modifier ownerOf(uint256 _tweetId){
+        require(tweetToOwner[_tweetId] == msg.sender,
+        "This action can only be performed by the owner of the tweet"
+        );
+        _;
+    }
 
     /**
     * @dev A function that creates a new Tweet given the text
@@ -20,29 +29,42 @@ contract TwitterClone is Ownable {
     function createTweet(string memory _tweetText) public {
         tweets.push(Tweet(_tweetText, false));
         uint256 tweetId = tweets.length -1;
-        emit NewTweet(tweetId, _tweetText);
+        tweetToOwner[tweetId] = msg.sender;
+        ownerTweetCount[msg.sender]++;
+        emit NewTweet(tweetId, _tweetText, msg.sender);
     }
 
     /**
     * @dev A function that edits a Tweet text with a new text. Only Tweet owner can edit a tweet.
-    * @param _tweet The tweet to be edited
+    * @param _tweetId The ID of the tweet to be edited
     * @param _newText The new text for the Tweet after edition
      */
-    function editTweet(Tweet memory _tweet, string memory _newText) external view onlyOwner {
-        _tweet.tweetText = _newText;
+    function editTweet(uint256 _tweetId, string memory _newText) external ownerOf(_tweetId) {
+        tweets[_tweetId].tweetText = _newText;
     }
 
     /**
     * @dev A function that mark a Tweet as deleted. Only Tweet owner can edit a tweet.
-    * @param _tweet The tweet to be deleted
+    * @param _tweetId The ID of the tweet to be deleted
      */
-
-    function deleteTweet(Tweet memory _tweet) external view onlyOwner {
-        _tweet.deleted = true;
+    function deleteTweet(uint256 _tweetId) external ownerOf(_tweetId) {
+        tweets[_tweetId].deleted = true;
     }
 
     function getTweetsLength() public view returns(uint count) {
         return tweets.length;
+    }
+
+    function getTweetsByOwner(address _owner) external view returns(uint[] memory) {
+        uint256[] memory result = new uint256[](ownerTweetCount[_owner]);
+        uint256 counter = 0;
+        for (uint256 i = 0; i< tweets.length; i++){
+            if (tweetToOwner[i] == _owner){
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
     }
 
 }
--- test/TwitterClone.test.js
+++ test/TwitterClone.test.js
@@ -2,6 +2,12 @@ require("chai").should();
 
 const TwitterClone = artifacts.require("TwitterClone");
 const truffleAssert = require("truffle-assertions");
+const {
+    // BN,           // Big Number support
+    // constants,    // Common constants, like the zero address and largest integers
+    expectEvent,  // Assertions for emitted events
+    expectRevert, // Assertions for transactions that should fail
+    } = require('@openzeppelin/test-helpers');
 
 contract("TwitterClone", (accounts) => {
     let owner = accounts[0];
@@ -11,53 +17,89 @@ contract("TwitterClone", (accounts) => {
         this.TwitterClone = await TwitterClone.new();
     });
 
-    describe("Creating Tweet", () => {
+    describe("CryptoTwitter tests", () => {
         it("Should create a new Tweet from a Text", async () => {
-            // console.log(this.TwitterClone.tweets)
-            this.TwitterClone.createTweet("First Tweet");
-            // console.log(this.TwitterClone.tweets.length);
+            this.TwitterClone.createTweet("First Tweet", {from: owner});
             firstTweet = await this.TwitterClone.tweets.call([0]);
-            // console.log(firstTweet);
             firstTweet.tweetText.should.equal("First Tweet");
         });
-    });
 
-    // Test is not working as the tweet created doesn't take the "owner" parameter. To be invesigated.
-    describe("Owner editing his own Tweet", () => {
+
         it("Should edit a Tweet text when the requester is the owner of the Tweet", async () => {
             this.TwitterClone.createTweet("First Tweet", {from: owner} );
-            console.log(this.TwitterClone.getTweetsLength);
-            firstTweet = await this.TwitterClone.tweets.call([0]);
-            // console.log(firstTweet);
             try {
-                this.TwitterClone.editTweet(firstTweet, "EDITED First Tweet", {from: owner});
+                this.TwitterClone.editTweet(0, "EDITED First Tweet", {from: owner});
                 editedTweet = await this.TwitterClone.tweets.call([0]);
-                // console.log(editedTweet);
                 editedTweet.tweetText.should.equal("EDITED First Tweet");
             } catch (e) {
-                console.log(`${owner} is not the owner`)
-                editedTweet = await this.TwitterClone.tweets.call([0]);
-                editedTweet.tweetText.should.equal("EDITED First Tweet");
-                // console.log(editedTweet);
-                
-            }
-            
+                console.log(`${owner} is not the owner`);                
+            }            
         });
-    });
 
-    describe("Other user trying to edit a Tweet he didn't create", () => {
+
+
         it("Should throw an error as modifier is not the owner of the Tweet", async () => {
             this.TwitterClone.createTweet("First Tweet", {from: owner} );
-            console.log(this.TwitterClone.tweets.length);
-            firstTweet = await this.TwitterClone.tweets.call([0]);
-            console.log(firstTweet);
-            await truffleAssert.reverts(
-                this.TwitterClone.editTweet(firstTweet, "EDITED First Tweet", {from: nonOwner}),
-                "VM Exception while processing transaction: revert"
+            try{
+            await expectRevert(
+                this.TwitterClone.editTweet(0, "EDITED First Tweet", {from: nonOwner}),
+                "This action can only be performed by the owner of the tweet"
             );
+            } catch (e){
+                console.log(e)
+            }
         });
-    });
+
+
+
+        it("Check that tweet list is of the correct length", async () => {
+            this.TwitterClone.createTweet("Tweet1");
+            this.TwitterClone.createTweet("Tweet2");
+            this.TwitterClone.createTweet("Tweet3");
+            this.TwitterClone.createTweet("Tweet4");
+            this.TwitterClone.createTweet("Tweet5");
+            let tweetL = await this.TwitterClone.getTweetsLength.call();
+            tweetL.toNumber().should.equal(5);
+        });
 
         // TODO test for deleting. This should be pretty straightforward if the Edit one is fixed.
 
+        it("Should set a Tweet delete attribute to true when the requester is the owner of the Tweet", async () => {
+            this.TwitterClone.createTweet("First Tweet", {from: owner} );
+            try {
+                this.TwitterClone.deleteTweet(0, {from: owner});
+                editedTweet = await this.TwitterClone.tweets.call([0]);
+                editedTweet.deleted.should.equal(true);
+            } catch (e) {
+                console.log(`${owner} is not the owner`);                
+            }
+        });   
+
+
+
+
+        it("Should throw an error as deleter is not the owner of the Tweet", async () => {
+            this.TwitterClone.createTweet("First Tweet", {from: owner} );
+            try{
+            await expectRevert(
+                this.TwitterClone.deleteTweet(0, {from: nonOwner}),
+                "This action can only be performed by the owner of the tweet"
+            );
+            } catch (e){
+                console.log(e)
+            }
+        });
+
+        // it('emits a Transfer event on successful transfers', async function () {
+        //     this.TwitterClone.createTweet("First Tweet", {from: owner} );
+        
+        //     // Event assertions can verify that the arguments are the expected ones
+        //     expectEvent(Result, {
+        //         '0': 'First Tweet',
+        //         '1': false,
+        //         tweetText: 'First Tweet',
+        //         deleted: false
+        //       });
+        // });
+    });
 });
